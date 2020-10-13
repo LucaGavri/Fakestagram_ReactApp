a9250e7a494004935428c3c0e46c0bcbbd4af04f
Lukasz Gawrys <l.gawrys@cheil.com>
2020-10-13 22:23:15 +0200

    first stage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div>
-      Hello react
-    </div>
-  );
+    return (
+        <div>
+            <span className="title">faKebook</span>
+            <div className="sth">ok lets do sth!</div>
+        </div>
+    );
 }
 
 export default App;
